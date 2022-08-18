682c716c32fc11b51f7d65f3970cec166a650eac
nickorrnah <icnicorr@gmail.com>
2022-08-17 23:35:13 -0700

    double_bar_chart.R complete.

--- twitch_growth/app/double_bar_chart.R
+++ twitch_growth/app/double_bar_chart.R
@@ -4,32 +4,37 @@ library (dplyr)
 library(plotly)
 library(stringr)
 
-twitch_df <- read.csv("data/data.csv")
+twitch_df <- read.csv("twitch_growth/data/data.csv")
 
 
-get_hours_watched <- function(yr) {
+get_hours_watched <- function(yr, type) {
   hours_watched_df <- twitch_df %>% filter(str_detect(twitch_df$Month, yr))
   hours_watched_df <- hours_watched_df$Hours.watched..mins. / 60
+  
+  if (type == 0) {
+    hours_watched_df <- head(hours_watched_df, 6)
+  } else {
+    hours_watched_df <- tail(hours_watched_df, 6)
+  }
   return(sum(hours_watched_df))
 }
 
+year_growth_front <- c(get_hours_watched("2017", 0),
+                       get_hours_watched("2018", 0),
+                       get_hours_watched("2019", 0),
+                       get_hours_watched("2020", 0),
+                       get_hours_watched("2021", 0))
 
-year_growth <- c((get_hours_watched("2017") - get_hours_watched("2016")) / (get_hours_watched("2016") * 100), 
-                 (get_hours_watched("2018") - get_hours_watched("2017")) / (get_hours_watched("2017") * 100),
-                 (get_hours_watched("2019") - get_hours_watched("2018")) / (get_hours_watched("2018") * 100), 
-                 (get_hours_watched("2020") - get_hours_watched("2019")) / (get_hours_watched("2019") * 100),
-                 (get_hours_watched("2021") - get_hours_watched("2020")) / (get_hours_watched("2020") * 100), 
-                 (get_hours_watched("2022") - get_hours_watched("2021")) / (get_hours_watched("2021") * 100))
+year_growth_back <- c(get_hours_watched("2017", 1),
+                      get_hours_watched("2018", 1),
+                      get_hours_watched("2019", 1),
+                      get_hours_watched("2020", 1),
+                      get_hours_watched("2021", 1))
 
-years <- c(2017, 2018, 2019, 2020, 2021, 2022)
+Year <- c(2017, 2018, 2019, 2020, 2021)
 
-data <- data.frame(years, year_growth)
+data <- data.frame(years, year_growth_front, year_growth_back)
 
-data$years <- factor(data$years, levels = data[["years"]])
-
-fig <- plot_ly(data, x = ~years, y = ~year_growth, name = 'Year over Year growth', type = 'scatter', mode = 'lines',
-               line = list(color = 'rgb(205, 12, 24)', width = 4)) 
-
-fig <- fig %>% layout(title = "Hours Streamed and Watched by Month",
-                      xaxis = list(title = "Year"),
-                      yaxis = list (title = "Hours (millions)"))
+bar_fig <- plot_ly(data, x = ~Year, y = ~year_growth_front, type = 'bar', name = 'Front Half (January-June)')
+bar_fig <- bar_fig %>% add_trace(y = ~year_growth_back, name = 'Back Half (July-December)')
+bar_fig <- bar_fig %>% layout(yaxis = list(title = 'Hours Watched'), barmode = 'group')
