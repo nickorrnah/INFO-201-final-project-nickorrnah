3b3d1636267322c23f364b7b38c5d27228d44fd8
nickorrnah <60363305+nickorrnah@users.noreply.github.com>
2022-08-19 01:11:39 -0700

    Update server.R

--- twitch_growth/app/server.R
+++ twitch_growth/app/server.R
@@ -1,5 +1,20 @@
 # Server 
-server <- function(input, output, session) {
+server <- function(input, output) {
  
   output$landing <- renderImage(list(src = "www/landing_image.jpg", width = "100%"), deleteFile = F)
+  
+  output$view_pie_chart <- renderPlotly({ build_pie(input$range[1], input$range[2]) })
+  
+  bar_selection <- reactive(input$bar_selection)
+  
+  output$double_bar_chart <- renderPlotly({ 
+    if (bar_selection() == "watched") {
+      hrs_watched_bar_fig
+    } else {
+      hrs_streamed_bar_fig
+    }
+  })
+  
+    
+  
 }
