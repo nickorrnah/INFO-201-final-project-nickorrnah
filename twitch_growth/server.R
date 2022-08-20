f82c40871e8e6126bf550ed06af67fa804faef0f
nickorrnah <60363305+nickorrnah@users.noreply.github.com>
2022-08-19 20:01:34 -0700

    Update server.R

--- twitch_growth/server.R
+++ twitch_growth/server.R
@@ -2,6 +2,7 @@
 server <- function(input, output) {
   
   output$landing <- renderImage(list(src = "www/landing_image.jpg", width = "100%"), deleteFile = F)
+  output$conclusion_image <- renderImage(list(src = "www/conclusion_image.jpg", width = "100%"), deleteFile = F)
   
   output$view_pie_chart <- renderPlotly({ build_pie(input$range[1], input$range[2]) })
   
@@ -20,6 +21,7 @@ server <- function(input, output) {
   
   output$viewership <- renderText(viewership_text)
   
+  output$conclusion_text <- renderText(conclusion_text)
   
   output$hours <- renderText(if (bar_selection() == "watched") {
     hours_watched_text
