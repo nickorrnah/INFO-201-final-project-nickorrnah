eab3913f1db484cd536b8948c5012f4c5ec96bd9
nickorrnah <60363305+nickorrnah@users.noreply.github.com>
2022-08-19 19:12:43 -0700

    Update ui.R

--- twitch_growth/ui.R
+++ twitch_growth/ui.R
@@ -6,84 +6,94 @@ library(shinyWidgets)
 library(plotly)
 library(bslib)
 
-source('twitch_growth/double_bar_chart.R')
-source('twitch_growth/views_pie_chart.R')
+source('double_bar_chart.R')
+source('views_pie_chart.R')
+source('text.R')
 
 ui <- fluidPage(
   
   navbarPage( "The Growth of Twitch",
+              
+              theme = bs_theme(bootswatch = "darkly"),
+              
+              inverse = TRUE,
+              
+              #Home Page Panel
+              tabPanel("Home",
+                       
+                       fluidRow(width = 12, hr(), hr(),
+                                
+                                imageOutput("landing",
+                                            
+                                            width = "99%",
+                                            height = "10%"
+                                            
+                                ),
+                                verbatimTextOutput("intro")
+                                
+                       )     
+                       
+              ),
+              
+              tabPanel("Hours Watched",
+                       
+                       selectInput("bar_selection",
+                                   
+                                   label = "Hour Type",
+                                   choices = c("Watched Hours" = "watched",
+                                               "Streamed Hours" = "stream")
+                       ),   
+                       
+                       mainPanel(plotlyOutput("double_bar_chart"),
+                                 verbatimTextOutput("hours"))
+                       
+              ),
+              
+              tabPanel("Viewership",
+                       
+                       sidebarLayout(
+                         
+                         sidebarPanel(
+                           
+                           sliderInput("range", 
+                                       label = "Years of interest:",
+                                       min = 2016, max = 2022, value = c(2016, 2021)
+                           )
+                           
+                         ),
+                         
+                         mainPanel(plotlyOutput("view_pie_chart"),
+                         verbatimTextOutput("viewership")
+                         )
+                       ) 
+                       
+              ),
+              
+              tabPanel("Streamers"
+                       
+                       #h1("Twitch Channel Affiliation Status"),
+                       #selectInput(
+                         #inputId = "year1",
+                         #label = "Select first year",
+                         #choices = twitchdf$Year
+                       #),
+                       #selectInput(
+                         #inputId = "year2",
+                         #label = "Select second year",
+                         #choices = twitchdf$Year
+                       #),
+              
+              #mainPanel(
+                #tabsetPanel(
+                  #tabPanel("Plot", plotOutput(outputId = "1year")),
+                  #tabPanel("Table", tableOutput(outputId = "2year"))
+                #) 
+              #)
+                       
+              ),
   
-    theme = bs_theme(bootswatch = "darkly"),
-    
-    inverse = TRUE,
-    
-    #Home Page Panel
-    tabPanel("Home",
-          
-      fluidRow(width = 12, hr(), hr(),
-        
-        imageOutput("landing",
-                    
-          width = "100%",
-          height = "10%"
-          
-        )
-        
-      )     
-             
-    ),
-    
-    tabPanel("Hours Watched",
-
-      selectInput("bar_selection",
-                  
-        label = "Hour Type",
-        choices = c("Watched Hours" = "watched",
-                    "Streamed Hours" = "stream")
-      ),   
-      
-      mainPanel(plotlyOutput("double_bar_chart"))
-             
-    ),
-    
-    tabPanel("Viewership",
-             
-      sidebarLayout(
-               
-        sidebarPanel(
-                 
-          sliderInput("range", 
-            label = "Years of interest:",
-            min = 2016, max = 2022, value = c(2018, 2020)
-          )
-                 
-        ),
-               
-        mainPanel(plotlyOutput("view_pie_chart"))
-               
-     ) 
-             
-    ),
-    
-    tabPanel("Streamers",
-             
-      h1("Twitch Channel Affiliation Status"),
-        selectInput(
-          inputId = "year1",
-          label = "Select first year",
-          choices = twitchdf$Year
-        ),
-        selectInput(
-          inputId = "year2",
-          label = "Select second year",
-          choices = twitchdf$Year
-        )
-      ),
-      mainPanel(
-        tabsetPanel(
-          tabPanel("Plot", plotOutput(outputId = "1year")),
-          tabPanel("Table", tableOutput(outputId = "2year"))
-        )             
-      )
+              
+              tabPanel("Conclusions")
+              
   )
 )
